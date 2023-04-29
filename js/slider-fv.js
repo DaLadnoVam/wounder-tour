ace40e714e8aca47be6c6dd328076a810c9957a2
DaLadnoVam <daladnovam2@gmail.com>
2023-04-29 13:48:13 +0300

    'fix'

--- js/slider-fv.js
+++ js/slider-fv.js
@@ -5,22 +5,11 @@ const swiper1 = new Swiper(".fv-swiper", {
   direction: "vertical",
   loop: true,
   spaceBetween: 0,
+  touchRatio: 0,
   autoplay: {
-    delay: 5000,
+    delay: 3000,
   },
   navigation: false,
   scrollbar: false,
-  breakpoints: {
-    // when window width is >= 320px
-    310: {
-      allowTouchMove: false,
-    },
-    768: {
-      allowTouchMove: true,
-    },
-  },
-  // If we need pagination
-  // pagination: {
-  //   el: ".swiper-pagination",
-  // },
+  pagination: false,
 });
